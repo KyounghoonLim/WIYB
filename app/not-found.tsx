76dcfe4a7bdc537d62e951d722ebadfdb850fb2b
KyounghoonLim <mogies@naver.com>
2024-08-09 19:48:08 +0900

    [feat]: notfound 페이지 / 미들웨어 수정
    유도리있게 이외의 것도 추가 가능

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import Button from '@/src/components/button/Button'
+import { PATH } from '@/src/constants/path.constant'
+import Link from 'next/link'
+import React from 'react'
 
 export default function NotFoundPage() {
-  return <div>not-found</div>;
+  return (
+    <main className="CONTENT-CONTAINER flex-col-center gap-10">
+      <h1 className="typograph-24 font-bold">존재하지 않는 페이지입니다.</h1>
+      <Link href={PATH.MAIN}>
+        <Button text="메인으로 가기" />
+      </Link>
+    </main>
+  )
 }
--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,37 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { localeMiddleware } from "./src/middleware/locale.middleware";
-import { navigationGuardMiddleware } from "./src/middleware/navigationGuard.middleware";
-import { COOKIE_KEYS } from "./src/constants/cookie.constant";
+import { NextResponse, type NextRequest } from 'next/server'
+import { localeMiddleware } from './src/middleware/locale.middleware'
+import { navigationGuardMiddleware } from './src/middleware/navigationGuard.middleware'
+import { COOKIE_KEYS } from './src/constants/cookie.constant'
+import { PATH } from './src/constants/path.constant'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const { nextUrl } = request;
+  const { nextUrl } = request
 
-  /// 사용자의 요청에 대해 갈 수 있는지 판단 ///
-  const result_1 = await navigationGuardMiddleware(request);
-  /// 앞선 모듈들의 결과에서 locale 을 추가 ///
-  const { url, cookie } = await localeMiddleware(request, result_1);
-  /// 최종 응답 처리 ///
-  nextUrl.pathname = url.path;
-  nextUrl.search = url.search;
+  if (nextUrl.pathname === '/') {
+    nextUrl.pathname = PATH.MAIN
+    return NextResponse.redirect(nextUrl)
+  } else {
+    /// 사용자의 요청에 대해 갈 수 있는지 판단 ///
+    const result_1 = await navigationGuardMiddleware(request)
+    /// 앞선 모듈들의 결과에서 locale 을 추가 ///
+    const { url, cookie } = await localeMiddleware(request, result_1)
+    /// 최종 응답 처리 ///
+    nextUrl.pathname = url.path
+    nextUrl.search = url.search
 
-  const response = url.redirect ? NextResponse.redirect(nextUrl) : NextResponse.next();
-  if (cookie) {
-    const { key, value, options } = cookie;
-    response.cookies.set(key, value, options);
+    const response = url.redirect ? NextResponse.redirect(nextUrl) : NextResponse.next()
+    if (cookie) {
+      const { key, value, options } = cookie
+      response.cookies.set(key, value, options)
+    }
+    response.cookies.set(COOKIE_KEYS.REQUEST_TIME, String(Date.now()))
+
+    return response
   }
-  response.cookies.set(COOKIE_KEYS.REQUEST_TIME, String(Date.now()));
-
-  return response;
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/((?!images|_next).*)"],
-};
+  matcher: ['/((?!images|_next).*)'],
+}
