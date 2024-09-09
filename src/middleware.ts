19f42b5e387721218a00ec25a3ee199afcae0412
KyounghoonLim <mogies@naver.com>
2024-09-09 17:07:39 +0900

    [fix]: safari Inter 폰트 랜더링 차이로 apple-system 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,6 @@ import { PATH } from 'constants/path.constant'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  console.log(request.nextUrl.href)
   return await navigationGuard(request)
 }
 
